42e98b35b2b7f658db99bef56e7fab32fa07aa46
zehnzen <zehna-berg@live.nl>
2016-01-15 13:51:36 +0100

    mailbox geïmplementeerd in een klasse en zorgt voor correcte communicatie tussen uart tast en sensor task

--- Programmacode/Testing2/test.cpp
+++ Programmacode/Testing2/test.cpp
@@ -16,108 +16,117 @@
 
 using namespace std;
 
-class uart_task : public RTOS::task {
+struct Requests{
+	uint8_t first, second;
+};
+
+class UartCommunicator {
 public:
-   uart_task(short prio, const char * name):
-		RTOS::task(prio, name),
-		commandCh(this, "channel")
-		{
-			receivePool.write(false);
-		}	
-			
-	void writeCommand(uint8_t command){
-		commandCh.write(command);
+	UartCommunicator():
+		mailboxIn("mailbox"),
+		mailboxOut("mailbox")
+	{}
+	
+	void writeCommand(Requests req){
+		mailboxIn.write(req);
 	}
 	
-	bool waitReply(){
-		bool b = receivePool.read();
-		if(b){
-			receivePool.write(false);
-		}
-		return b;
+	Requests readCommand(){ 
+		return mailboxIn.read();
 	}
+	
+	void writeReply(int n){
+		mailboxOut.write(n);
+	}
+	
+	int readReply(){
+		return mailboxOut.read();
+	}
+	
+private:
+	RTOS::mailbox<Requests> mailboxIn;
+	RTOS::mailbox<int> mailboxOut;
+};
+
+class uart_task : public RTOS::task{
+public:
+	uart_task(short prio, const char * name, UartCommunicator* uartCom):
+		RTOS::task(prio, name),
+		uartCom(uartCom)
+		{}
 
 private:
 	LibSerial serial;
-	RTOS::channel <uint8_t, 10> commandCh;
-	RTOS::pool <bool> receivePool;
-	int n;
+	UartCommunicator* uartCom;
 	int reply = 0;
 	
 private:
 	void writeSerial(){
-		receivePool.write(false);
-		wait(commandCh);
-		serial.writeChar(commandCh.read());
-		wait(commandCh);
-		serial.writeChar(commandCh.read());
+		Requests req = uartCom->readCommand();
+		serial.writeChar(req.first);
+		serial.writeChar(req.second);
 		cout << "klaar met schrijven" << endl;
-		while(serial.peek() < 1){
-			sleep(100);
+		while(serial.peek() < 2){
+			sleep(1000);
 		}
 		serial.read(&reply,2);
-		cout << hex << reply << endl;
-		receivePool.write(true);
+		uartCom->writeReply(reply);
 	}
 	
    void main (void) {
-   int err = serial.open("/dev/ttyAMA0", 9600);
-   if(err < 1){
-		if(err == -1){
-			std::cout << "error while opening device\n";
+	   int err = serial.open("/dev/ttyAMA0", 9600);
+	   if(err < 1){
+			if(err == -1){
+				std::cout << "error while opening device\n";
+			}
+			else if(err == -2){
+				std::cout << "unable to set non-blocking mode\n";
+			}
+			else if(err == -3){
+				std::cout << "speed (bauds) not recognized\n";
+			}
+			else if(err == -4){
+				std::cout << "unable to set baud rate\n";
+			}
+			else if(err == -5){
+				std::cout << "unable to set flags\n";
+			}	
 		}
-		else if(err == -2){
-			std::cout << "unable to set non-blocking mode\n";
+		else{
+			cout << "schrijft nu" << endl;
+			serial.writeChar(MACHINE_REQ);
+			serial.writeChar(START_CMD);
+			cout << "klaar met schrijven" << endl;
+			while(serial.peek() < 2){
+				sleep(6000);
+			}
+			serial.read(&reply,2);
+			cout << hex << reply << endl;
 		}
-		else if(err == -3){
-			std::cout << "speed (bauds) not recognized\n";
+		for (;;) {
+			writeSerial();
 		}
-		else if(err == -4){
-			std::cout << "unable to set baud rate\n";
-		}
-		else if(err == -5){
-			std::cout << "unable to set flags\n";
-		}	
 	}
-	else{
-		cout << "schrijft nu" << endl;
-		serial.writeChar(MACHINE_REQ);
-		serial.writeChar(START_CMD);
-		cout << "klaar met schrijven" << endl;
-		while(serial.peek() < 2){
-			sleep(6000);
-		}
-		serial.read(&reply,2);
-		cout << hex << reply << endl;
-	}
-        for (;;) {
-            if (n % 10000 == 0) {
-                cout << "i: " << (n / 10000) << endl;
-				writeSerial();
-            }
-            n += 1;
-            sleep(500 US);
-        }
-   }
 };
 
 class Sensor_task : public RTOS::task{
 	public:
-	Sensor_task(short prio, const char* name, uart_task* uart):
+	Sensor_task(short prio, const char* name, UartCommunicator* uart):
 		RTOS::task(prio, name),
-		uart(uart)
+		uartCom(uart)
 		{}
 		
 	private:
-		uart_task* uart;
+		UartCommunicator* uartCom;
 	
 	private:
 		void main(void){
 			for (;;) {
 				cout << "schrijf tempreq in queue" << endl;
-				uart->writeCommand(TEMPERATURE_REQ);
-				uart->writeCommand(STATUS_CMD);
+				Requests req = { TEMPERATURE_REQ, STATUS_CMD};
+				uartCom->writeCommand(req);
 				sleep(2000 * 1000);
+				cout << uartCom->readReply() << endl;
 			}	
 		}
 };
@@ -161,24 +170,18 @@ public:
 		}
 	}
 };
-	
-			
-void writeTest () {
-	cout << "runt";
-	uart_task uart(20, "UART");
-	Sensor_task sensor(30, "Sensor", &uart);
-	Websocket_task ws;
-	thread x(&Websocket_task::run,&ws);
-	RTOS::run();
-}
-
-
 
 /******************************************************************************\
  main routine
 \******************************************************************************/
 int main( void )
 {
-   writeTest();
-   return 0;
+	cout << "runt";
+	UartCommunicator uartCom;
+	uart_task uart(20, "UART", &uartCom);
+	Sensor_task sensor(30, "Sensor", &uartCom);
+	Websocket_task ws;
+	thread x(&Websocket_task::run,&ws);
+	RTOS::run();
+	return 0;
 }
