2ae8df8a5f48b85fd57106816bb27d3a31aff290
zehnzen <zehna-berg@live.nl>
2016-01-18 14:07:22 +0100

    Alle sensoren zijn toegevoegd met commando's

--- Programmacode/Testing2/Sensor.h
+++ Programmacode/Testing2/Sensor.h
@@ -1,16 +1,22 @@
 #include "uartCommunicator"
 
-class Sensor{
+class WmHardware{
+protected:
+	WmHardware(UartCommunicator* uart):
+		uartCom(uart){}
+
+	UartCommunicator* uartCom;
+};
+
+class Sensor : public WmHardware{
 public:
-	Sensor(UartCommunicator* uart):
-		uartCom{uart}
-		{}
-		
-	void update(){
+	virtual void update(){
 	}
 	
 protected:
-	UartCommunicator* uartCom
+	Sensor(UartCommunicator* uart):
+		WmHardware{uart}
+		{}
 };
 
 class TempSensor : public Sensor{
@@ -65,24 +71,128 @@ class Trommel : public Sensor{
 public:
 	Trommel(UartCommunicator* uart):
 		Sensor{uart}{}
+		
+	void update(){
+		getRPM();
+	}
 	
-	void setRPM(int i){
-		bool reverse = false;
+	void setRPM(unsigned int i, bool clockwise){
 		if(i > 1600){ i = 1600;}
-		if(i < -1600){ i = -1600;}
-		if(i < 0){reverse = true;}
 		i = i / 25;
-		uint8_t rpm = ((reverse) ? i | 0x80 : i);
+		uint8_t rpm = ((clockwise) ? i : i | 0x80);
 		
 		Requests req = {SET_RPM_REQ, rpm};
 		uartCom->writeCommand(req);
-		cout << "WMStatus = " << uartCom->readReply() << endl;
+		cout << "RPM = " << uartCom->readReply() << endl;
 	}
 	
 	void getRPM(){
 		Requests req = {GET_RPM_REQ, STATUS_CMD};
 		uartCom->writeCommand(req);
-		cout << "WMStatus = " << uartCom->readReply() << endl;
+		cout << "RPMSet = " << uartCom->readReply() << endl;
 	}
 };
+
+class DoorLock : public WmHardware{
+public:
+	DoorLock(UartCommunicator* uart):
+		WmHardware{uart}{}
+	
+	void getDoorLock(){
+		Requests req = {DOOR_LOCK_REQ, STATUS_CMD};
+		uartCom->writeCommand(req);
+		cout << "DoorLock = " << uartCom->readReply() << endl;
+	}
+	
+	void setDoorLock(bool lock){
+		Requests req = {DOOR_LOCK_REQ, ((lock) ? LOCK_CMD : UNLOCK_CMD)};
+		uartCom->writeCommand(req);
+		cout << "DoorLockSet = " << uartCom->readReply() << endl;
+	}
+};
+
+class WaterValve : public WmHardware{
+public:
+	WaterValve(UartCommunicator* uart):
+		WmHardware{uart}{}
+		
+	void getWaterValve(){
+		Requests req = {WATER_VALVE_REQ, STATUS_CMD};
+		uartCom->writeCommand(req);
+		cout << "WaterValve = " << uartCom->readReply() << endl;
+	}
+	
+	void setWaterValve(bool open){
+		Requests req = {WATER_VALVE_REQ, ((open) ? OPEN_CMD : CLOSE_CMD)};
+		uartCom->writeCommand(req);
+		cout << "WaterValveSet = " << uartCom->readReply() << endl;
+	}
+};
+
+class Pump : public WmHardware{
+public:
+	Pump(UartCommunicator* uart):
+		WmHardware{uart}{}
+		
+	void getPump(){
+		Requests req = {PUMP_REQ, STATUS_CMD};
+		uartCom->writeCommand(req);
+		cout << "PumpStatus = " << uartCom->readReply() << endl;
+	}
+	
+	void setPump(bool on){
+		Requests req = {PUMP_REQ, ((on) ? ON_CMD : OFF_CMD)};
+		uartCom->writeCommand(req);
+		cout << "PumpStatusSet = " << uartCom->readReply() << endl;
+	}
+};
+
+class SoapDispenser : public WmHardware{
+public:
+	SoapDispenser(UartCommunicator* uart):
+		WmHardware{uart}{}
+	
+	void getSoapDispenser(){
+		Requests req = {SOAP_DISPENSER_REQ, STATUS_CMD};
+		uartCom->writeCommand(req);
+		cout << "SoapDispenser = " << uartCom->readReply() << endl;
+	}
+	
+	void setSoapDispenser(bool open){
+		Requests req = {SOAP_DISPENSER_REQ, ((open) ? OPEN_CMD : CLOSE_CMD)};
+		uartCom->writeCommand(req);
+		cout << "SoapDispenserSet = " << uartCom->readReply() << endl;
+	}	
+};
+
+class HeatingUnit : public WmHardware{
+public:
+	HeatingUnit(UartCommunicator* uart):
+		WmHardware{uart}{}
+	
+	void getHeatingUnit(){
+		Requests req = {HEATING_UNIT_REQ , STATUS_CMD};
+		uartCom->writeCommand(req);
+		cout << "SoapDispenser = " << uartCom->readReply() << endl;
+	}
+	
+	void setHeatingUnit(bool on){
+		Requests req = {HEATING_UNIT_REQ , ((on) ? OPEN_CMD : CLOSE_CMD)};
+		uartCom->writeCommand(req);
+		cout << "SoapDispenserSet = " << uartCom->readReply() << endl;
+	}	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
